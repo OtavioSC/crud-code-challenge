f8acde55a6be812086e49fd4d4180ecc21bd1bac
Otávio Carmo <otavio.carmo2019@Outlook.com>
2024-03-05 16:37:50 -0300

    feat: add components to main page

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,9 +1,16 @@
-import Modal from "@/components/modal/Modal"
+import FormPost from "@/components/FormPost/FormPost";
+import Post from "@/components/Post/Post";
 
 export default function Page() {
   return (
-    <>
-      <Modal/>
-    </>
-  )
+    <main className="w-[800px] bg-white h-full">
+      <header className="h-[80px] bg-[#7695EC] flex items-center">
+        <h1 className="text-xl font-bold text-white pl-10">CodeLeap Network</h1>
+      </header>
+      <section className="flex flex-col justify-center pt-8 items-center">
+        <FormPost />
+				<Post/>
+      </section>
+    </main>
+  );
 }
